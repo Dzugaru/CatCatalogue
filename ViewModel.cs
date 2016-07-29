53c4758d94d6ae64c0c8b55aa213d0f0fdf58a1c
Dzugaru <dzugaru@gmail.com>
2016-07-30 00:12:39 +0300

    ---

--- ViewModel.cs
+++ ViewModel.cs
@@ -16,7 +16,7 @@ namespace CatCatalogue
     class ViewModel : BindableBase
     {
         string testText = "Alb";
-        ObservableCollection<int> testList;
+        ObservableCollection<object> testList;
 
         public string TestText
         {
@@ -30,10 +30,65 @@ namespace CatCatalogue
 
         public ViewModel()
         {
-            testList = new ObservableCollection<int>(new List<int>() { 1, 2, 3, 4, 5 });
+            var vendor1 = new Vendor("Тимотеи");
+            vendor1.products.Add(new Product() { Name = "Шампунь", Rating = 10 });
+
+            testList = new ObservableCollection<object>(new List<object>()
+            {
+                "A",
+                "Б",
+                vendor1
+            });    
             TestList = new ListCollectionView(testList);
 
             TestCommand = new DelegateCommand(() => TestText = "Bla");
+        }
+    }
+
+    class Vendor : BindableBase
+    {
+        string name;
+
+        public ObservableCollection<Product> products;
+
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+
+        public ICollectionView Products { get; private set; }
+        
+        public Vendor() {}
+
+        public Vendor(string name)
+        {
+            this.name = name;
+            this.products = new ObservableCollection<Product>();
+            Products = new ListCollectionView(this.products); //TODO: what if after deserialize?
+        }        
+    }
+
+    class Product : BindableBase
+    {
+        string name;
+        int rating;
+
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+
+        public int Rating
+        {
+            get { return rating; }
+            set { SetProperty(ref rating, value); }
+        }
+
+        public Product()
+        {
+
         }
     }
 
@@ -41,8 +96,15 @@ namespace CatCatalogue
     {
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
-            int x = (int)item;           
-            return (DataTemplate)((FrameworkElement)container).FindResource(x >= 3 ? "CompanyInfo" : "CapitalLetter");
+            string resourceName;
+            if (item is Vendor)
+                resourceName = "VendorInfo";
+            else if (item is string)
+                resourceName = "CapitalLetter";
+            else
+                throw new NotImplementedException();
+
+            return (DataTemplate)((FrameworkElement)container).FindResource(resourceName);
         }
     }
 }
